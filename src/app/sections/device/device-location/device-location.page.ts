57224fea35083a8bc2af45aae8443b7606197f69
coloz <coloz999@gmail.com>
2025-03-12 20:20:45 +0800

    test

--- src/app/sections/device/device-location/device-location.page.ts
+++ src/app/sections/device/device-location/device-location.page.ts
@@ -8,7 +8,7 @@ import { NoticeService } from "src/app/core/services/notice.service";
 import { DirectivesModule } from "src/app/core/directives/directives.module";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import * as maptalks from "maptalks";
+// import * as maptalks from "maptalks";
 import { toBD09, toWsg84 } from "src/app/core/functions/func";
 
 @Component({
@@ -62,68 +62,68 @@ export class DeviceLocationPage implements OnInit {
         this.devicePosition = await this.geolocationService.getDevicePosition(
           this.device,
         );
-        this.initMap(this.devicePosition);
+        // this.initMap(this.devicePosition);
       }
     });
   }
 
-  initMap(wsg04Position) {
-    let position = toBD09(wsg04Position);
-    this.centerPosition = position;
-    this.mymap = new maptalks.Map(this.map.nativeElement, {
-      center: position,
-      zoom: 13,
-      minZoom: 1,
-      maxZoom: 19,
-      spatialReference: {
-        projection: "baidu",
-      },
-      baseLayer: new maptalks.TileLayer("base", {
-        "urlTemplate":
-          "https://gss{s}.bdstatic.com/8bo_dTSlRsgBo1vgoIiO_jowehsv/tile/?qt=tile&x={x}&y={y}&z={z}&styles=pl&scaler=1&udt=20170927",
-        "subdomains": [0, 1, 2, 3],
-        "attribution":
-          '&copy; <a target="_blank" href="http://map.baidu.com">Baidu</a>',
-      }),
-    });
+  // initMap(wsg04Position) {
+  //   let position = toBD09(wsg04Position);
+  //   this.centerPosition = position;
+  //   this.mymap = new maptalks.Map(this.map.nativeElement, {
+  //     center: position,
+  //     zoom: 13,
+  //     minZoom: 1,
+  //     maxZoom: 19,
+  //     spatialReference: {
+  //       projection: "baidu",
+  //     },
+  //     baseLayer: new maptalks.TileLayer("base", {
+  //       "urlTemplate":
+  //         "https://gss{s}.bdstatic.com/8bo_dTSlRsgBo1vgoIiO_jowehsv/tile/?qt=tile&x={x}&y={y}&z={z}&styles=pl&scaler=1&udt=20170927",
+  //       "subdomains": [0, 1, 2, 3],
+  //       "attribution":
+  //         '&copy; <a target="_blank" href="http://map.baidu.com">Baidu</a>',
+  //     }),
+  //   });
 
-    let marker = new maptalks.Marker(
-      position,
-      {
-        "symbol": {
-          "markerFile": "./assets/img/map/marker.png",
-          "markerWidth": 40,
-          "markerHeight": 40,
-          "markerDx": 0,
-          "markerDy": 0,
-          "markerOpacity": 1,
-        },
-      },
-    );
-    // let userMarker = new maptalks.Marker(
-    //   position,
-    //   {
-    //     "symbol": {
-    //       "markerFile": "./assets/img/map/user.png",
-    //       "markerWidth": 30,
-    //       "markerHeight": 30,
-    //       "markerDx": 0,
-    //       "markerDy": 0,
-    //       "markerOpacity": 0.5,
-    //     },
-    //   },
-    // );
-    // new maptalks.VectorLayer("userMarker", userMarker).addTo(this.mymap);
-    new maptalks.VectorLayer("marker", marker).addTo(this.mymap);
-    this.mymap.on("moving", (e) => {
-      let newPosition = this.mymap.getCenter();
-      this.centerPosition = toWsg84([newPosition.x, newPosition.y]);
-      marker.setCoordinates(newPosition);
-    });
-    this.mymap.on("moveend", (e) => {
-      this.btnDisabled = false;
-    });
-  }
+  //   let marker = new maptalks.Marker(
+  //     position,
+  //     {
+  //       "symbol": {
+  //         "markerFile": "./assets/img/map/marker.png",
+  //         "markerWidth": 40,
+  //         "markerHeight": 40,
+  //         "markerDx": 0,
+  //         "markerDy": 0,
+  //         "markerOpacity": 1,
+  //       },
+  //     },
+  //   );
+  //   // let userMarker = new maptalks.Marker(
+  //   //   position,
+  //   //   {
+  //   //     "symbol": {
+  //   //       "markerFile": "./assets/img/map/user.png",
+  //   //       "markerWidth": 30,
+  //   //       "markerHeight": 30,
+  //   //       "markerDx": 0,
+  //   //       "markerDy": 0,
+  //   //       "markerOpacity": 0.5,
+  //   //     },
+  //   //   },
+  //   // );
+  //   // new maptalks.VectorLayer("userMarker", userMarker).addTo(this.mymap);
+  //   new maptalks.VectorLayer("marker", marker).addTo(this.mymap);
+  //   this.mymap.on("moving", (e) => {
+  //     let newPosition = this.mymap.getCenter();
+  //     this.centerPosition = toWsg84([newPosition.x, newPosition.y]);
+  //     marker.setCoordinates(newPosition);
+  //   });
+  //   this.mymap.on("moveend", (e) => {
+  //     this.btnDisabled = false;
+  //   });
+  // }
 
   async saveGeolocation() {
     let newConfig = {
