107db42103402cae346d3f67c233078073172891
coloz <coloz999@gmail.com>
2026-08-11 22:08:59 +0800

    移除esptouch相关功能

--- src/app/sections/adddevice/adddevice.module.ts
+++ src/app/sections/adddevice/adddevice.module.ts
@@ -17,8 +17,6 @@ const routes: Routes = [
       { path: '', component: AddDevicePage, },
       // { path: 'qrscanner', loadChildren: () => import('./qrscanner/qrscanner.module').then(m => m.QrscannerPageModule) },
       { path: ':deviceType', loadChildren: () => import('./guide/guide.module').then(m => m.GuidePageModule) },
-      // { path: ':deviceType/espTouch', loadChildren: () => import('./esptouch/esptouch.module').then(m => m.EsptouchPageModule) },
-      { path: ':deviceType/espTouchV2', loadChildren: () => import('./esptouch/esptouch.module').then(m => m.EsptouchPageModule) },
       // { path: ':deviceType/bleConfig', loadChildren: () => import('./bwconfig/bwconfig.module').then(m => m.BwconfigPageModule) },
       // { path: ':deviceType/apConfig', loadChildren: () => import('./apconfig/apconfig.module').then(m => m.ApconfigPageModule) },
       { path: ':deviceType/KeyConfig', loadChildren: () => import('./mqttkey/mqttkey.module').then(m => m.MqttkeyPageModule) },
